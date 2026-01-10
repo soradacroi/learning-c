d45ec1f0783879379fcb41f57b3c8d0f84bd0825
soradacroi <soradacroi@gmail.com>
2026-01-10 21:14:14 +0530

    variables

---
+++ abt_variables.c
@@ -0,0 +1,12 @@
+// we have char int short long and long long for integers
+// and we have floats for well floats decimals also double ig mm
+// we dont have boolean so we have to define it mm 
+// defining variables
+#include <stdio.h>
+
+int main() {
+    int this_is_an_interger;
+    int a = 10;
+    int b = 20, c = 20;
+    this_is_an_interger = b + c - a;
+    printf("%d", this_is_an_interger);}
