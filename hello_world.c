49924a66f22767b0182f0de4e0ff535d1e482148
soradacroi <soradacroi@gmail.com>
2026-01-10 20:55:07 +0530

    hello world

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+// learning c ccccccccccccccccccccccccccc
+// printing hello world yay
+// so comenting working like this "//" nice right 
+/* it also works like this its good if u want to
+do this well its great right??????????????????????????? */
+
+#include <stdio.h> // we use libs to do stuff like its for "printf" yay
+
+// the first code that will run will always run main 
+
+int main() { 
+    printf("Hello world\n");
+    return 0; // yas
+}
+
