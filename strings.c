cba52fbce9dfff1fb5c28b2a00654d3fab660f3d
soradacroi <soradacroi@gmail.com>
2026-01-11 13:57:20 +0530

    strings

---
+++ strings.c
@@ -0,0 +1,21 @@
+// strings
+
+#include <stdio.h>
+
+// strings are just array of characters 
+
+char * a = "this is a string"; // doing this can only be used to like read only string we cant modify this
+
+char b[] = "this is how u do for array";
+
+char c[] = "one"; // see this its normal
+char d[4] = "one"; /* but when u like define the length you have to add one because when we did array.c we saw that
+                     we need to have a \0 at the end to indecate the end of the string */
+
+
+int main(){
+    printf("%s\n", a);
+    printf("%s\n", b);
+    printf("%s\n", c);
+    printf("%s\n", d);
+}
